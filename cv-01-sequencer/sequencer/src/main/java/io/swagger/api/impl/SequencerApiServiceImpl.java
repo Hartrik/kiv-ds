383d41b73d33cf072b980d33925bde5f6b1d5f27
Patrik Harag <patrikharag@gmail.com>
2020-10-11 22:16:52 +0200

    Mock sequencer

--- cv-01-sequencer/sequencer/src/main/java/io/swagger/api/impl/SequencerApiServiceImpl.java
+++ cv-01-sequencer/sequencer/src/main/java/io/swagger/api/impl/SequencerApiServiceImpl.java
@@ -1,25 +1,20 @@
 package io.swagger.api.impl;
 
-import io.swagger.api.*;
-import io.swagger.model.*;
-
+import io.swagger.api.ApiResponseMessage;
+import io.swagger.api.SequencerApiService;
 import io.swagger.model.Operation;
-
-import java.util.List;
-import io.swagger.api.NotFoundException;
-
-import java.io.InputStream;
-
-import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
-
+import java.util.concurrent.atomic.AtomicLong;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
-import javax.validation.constraints.*;
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaJerseyServerCodegen", date = "2020-10-11T19:12:44.517Z")
+
 public class SequencerApiServiceImpl extends SequencerApiService {
+
+    private AtomicLong id = new AtomicLong();
+
     @Override
-    public Response sequencerPost(Operation operation, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
-        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
+    public Response sequencerPost(Operation operation, SecurityContext securityContext) {
+        long currentId = id.getAndIncrement();
+        System.out.println("Accepted op: " + currentId + " val: " + operation.getValue());
+        return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "Accepted")).build();
     }
 }
