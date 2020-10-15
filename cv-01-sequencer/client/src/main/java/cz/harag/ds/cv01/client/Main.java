7cb94966e9e135a21ebf9212c2ef9fe03c3091c1
Patrik Harag <patrikharag@gmail.com>
2020-10-15 23:32:44 +0200

    Improve client CLI

--- cv-01-sequencer/client/src/main/java/cz/harag/ds/cv01/client/Main.java
+++ cv-01-sequencer/client/src/main/java/cz/harag/ds/cv01/client/Main.java
@@ -9,29 +9,44 @@ import java.util.Random;
 
 /**
  * @author Patrik Harag
- * @version 2020-10-11
+ * @version 2020-10-15
  */
 public class Main {
 
-    private static final String BASE_PATH = "http://localhost:8080";
-
+    private static final String DEFAULT_BASE_PATH = "http://localhost:8080";
     private static final int DEFAULT_N = 100;
+
     private static final int MIN_VALUE = 10000;
     private static final int MAX_VALUE = 50000;
 
     public static void main(String[] args) {
-        if (args.length == 0) {
-            generate(DEFAULT_N);
+        String path = DEFAULT_BASE_PATH;
+        if (args.length > 0) {
+            path = args[0];
         } else {
-            generate(Integer.parseInt(args[0]));
+            System.out.println("Using default base path: " + path);
         }
+
+        int n = DEFAULT_N;
+        if (args.length > 1) {
+            try {
+                n = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                printUsage();
+                return;
+            }
+        } else {
+            System.out.println("Using default n: " + DEFAULT_N);
+        }
+
+        generate(path, n);
     }
 
-    private static void generate(int n) {
+    private static void generate(String path, int n) {
         System.out.println("Generating " + n + " entries...");
 
         ApiClient configuration = Configuration.getDefaultApiClient();
-        configuration.setBasePath(BASE_PATH);
+        configuration.setBasePath(path);
         DefaultApi apiInstance = new DefaultApi(configuration);
 
         Random random = new Random();
@@ -53,6 +68,10 @@ public class Main {
                 e.printStackTrace();
             }
         }
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: <sequencer base path> <number of operations>");
     }
 
 }
